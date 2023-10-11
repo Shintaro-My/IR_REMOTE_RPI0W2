b7332a3331c192bb86390bf1b6acd1c901a7268f
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-10-11 16:35:36 +0900

    update

--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ def _sys_exit(signal, frame):
     sys.exit()
 
 if __name__ == '__main__':
+    """
     ir = IRRP(file="ir.ircode", post=130, no_confirm=True)
     ir.Record(GPIO=18, ID="light:on")
     ir.stop()
+    """
+    ir = IRRP(file="test", no_confirm=True)
+    ir.Playback(GPIO=17, ID="1")
+    ir.stop()
+    
