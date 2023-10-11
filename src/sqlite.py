34d45a24a63846c5843db604eb6db82836c8b317
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-10-11 13:06:36 +0900

    update

--- src/sqlite.py
+++ src/sqlite.py
@@ -5,40 +5,47 @@ DB_NAME = 'database.db'
 
 class DB:
     def __init__(self, name=DB_NAME) -> None:
-        self.conn = sqlite3.connect(DB_NAME)
+        self.db_name = name
+        self.conn = None
+        self.cur = None
+        
+    def _setup(self):
+        self.conn = sqlite3.connect(self.db_name)
         self.cur = self.conn.cursor()
         
-    def run(self, queries):
-        results = [[v for v in self.cur.execute(query.strip())] for query in queries]
-        self.conn.commit()
-        return results
-    
-    def terminate(self):
+    def _terminate(self):
         self.cur.close()
         self.conn.close()
         
+    def run(self, queries):
+        results = []
+        try:
+            self._setup()
+            results = [[v for v in self.cur.execute(query.strip())] for query in queries]
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            self._terminate()
+        return results
+    
+        
 if __name__ == '__main__':
     db = DB()
-    try:
-        db.run([
-        """
-        CREATE TABLE IF NOT EXISTS Test(
-            Key   TEXT PRIMARY KEY,
-            Value INTEGER
-        )
-        """,
-        """
-        INSERT INTO Test (Key, Value)
-        VALUES (\"test\", 100)
-        ON CONFLICT(Key)
-        DO UPDATE SET Value=100
-        """
-        ])
-        results, = db.run(["SELECT * FROM Test"])
-        print(results)
+    db.run([
+    """
+    CREATE TABLE IF NOT EXISTS Test(
+        Key   TEXT PRIMARY KEY,
+        Value INTEGER
+    )
+    """,
+    """
+    INSERT INTO Test (Key, Value)
+    VALUES (\"test\", 100)
+    ON CONFLICT(Key)
+    DO UPDATE SET Value=100
+    """
+    ])
+    results, = db.run(["SELECT * FROM Test"])
+    print(results)
     
-    except Exception as e:
-        print(e)
-        
-    finally:
-        db.terminate()
