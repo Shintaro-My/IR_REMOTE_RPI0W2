7ec580bee405ec1985b01c65bae0feb5e880f53e
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-10-11 16:56:47 +0900

    update

--- src/main.py
+++ src/main.py
@@ -7,12 +7,20 @@ def _sys_exit(signal, frame):
     sys.exit()
 
 if __name__ == '__main__':
-    """
-    ir = IRRP(file="ir.ircode", post=130, no_confirm=True)
-    ir.Record(GPIO=18, ID="light:on")
-    ir.stop()
-    """
-    ir = IRRP(file="test", no_confirm=True)
-    ir.Playback(GPIO=17, ID="1")
-    ir.stop()
+    debug = 2
+    if debug == 0:
+        ir = IRRP(file="ir.ircode", post=130, no_confirm=True)
+        ir.Record(GPIO=18, ID="light:on")
+        ir.stop()
+    elif debug == 1:
+        ir = IRRP(file="test", no_confirm=True)
+        ir.Playback(GPIO=17, ID="1")
+        ir.stop()
+    elif debug == 2:
+        import pigpio, time
+        pi = pigpio.pi()
+        pi.set_mode(15, pigpio.OUTPUT)
+        pi.write(15, pigpio.HIGH)
+        time.sleep(2)
+        pi.stop()
     
