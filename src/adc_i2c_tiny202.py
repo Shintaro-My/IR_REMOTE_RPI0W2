950bf377c68e447619c1babf0c9d521f4205eb1e
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-11-08 11:29:36 +0900

    update

---
+++ src/adc_i2c_tiny202.py
@@ -0,0 +1,17 @@
+import smbus
+
+class ADC_I2C_TINY202:
+    def __init__(self, bus=11, addr=0x20) -> None:
+        self.bus = smbus.SMBus(bus)
+        self.addr = addr
+    def get(self, num=0):
+        self.bus.write_byte(self.addr, num)
+        return self.bus.read_word_data(self.addr, 0)
+    
+if __name__ == '__main__':
+    import time
+    adc = ADC_I2C_TINY202()
+    
+    while True:
+        print( adc.get() )
+        time.sleep(.25)
