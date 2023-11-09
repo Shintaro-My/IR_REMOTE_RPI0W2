b016e71d1243079fdfb21556b9c1b32742e68ec0
Tsuden-Miyahara <miyahara.tsuden@outlook.jp>
2023-11-09 11:47:29 +0900

    update

--- src/ir_string.py
+++ src/ir_string.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 from base64 import b64decode as bdec, b64encode as benc
 
 
-"""
 def _enc(n):
-    if n < 0 or int('F' * DIGIT, 16) <= n: raise Exception('out of range')
+    if n < 0 or 0xFFFFFF <= n: raise Exception('out of range')
     return benc( n.to_bytes(3, 'big') ).decode()
 
 def _dec(s):
@@ -19,21 +18,3 @@ def encode(data: list[int]):
 def decode(string: str):
     if len(string) % 4: raise Exception('invalid code')
     return [_dec( string[x:x+4] ) for x in range(0, len(string), 4)]
-"""
-
-
-def _enc(n):
-    if n < 0: raise Exception('out of range')
-    return benc( n.to_bytes((n.bit_length() + 7) // 8, 'big') ).decode()
-
-def _dec(s):
-    try:
-        return int.from_bytes( bdec( s.encode() ), 'big' )
-    except:
-        raise Exception('invalid code')
-
-def encode(data: list[int]):
-    return ":".join([_enc(n) for n in data])
-
-def decode(string: str):
-    return [_dec( s ) for s in string.split(':')]
