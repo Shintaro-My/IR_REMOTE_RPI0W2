3419e9634fc1553a74a9be9a0dc3177fc3745958
Shintaro-My <shintaro@seijim.onmicrosoft.com>
2023-11-13 16:45:12 +0900

    update

---
+++ shell_checkping.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd /home/pi/IR_REMOTE_RPI0W2
+for i in {1..5}
+do
+    sudo -H -u pi python3 checkping.py
+    if [ $? -eq 0 ]; then
+        echo "Success"
+        break
+    else
+        sleep 5
+    fi
+done
+if [ $i -eq 5 ]; then
+    echo "Retry failed"
+fi
